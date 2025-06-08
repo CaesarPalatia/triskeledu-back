7436ed7d48ee33da5e05d6a35df29bf976b7b423
César Palacios <ce.palacios@duocuc.cl>
2025-06-08 16:20:29 -0400

    Initial commit

---
+++ ms-courses/src/main/java/com/edutech/courses/mapper/CourseCategoryMapper.java
@@ -0,0 +1,11 @@
+package com.edutech.courses.mapper;
+
+import com.edutech.common.dto.CourseCategoryDTO;
+import com.edutech.courses.entity.CourseCategory;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CourseCategoryMapper {
+    CourseCategoryDTO toDTO(CourseCategory entity);
+    CourseCategory toEntity(CourseCategoryDTO dto);
+}
---
+++ ms-courses/src/main/java/com/edutech/courses/mapper/CourseCommentMapper.java
@@ -0,0 +1,11 @@
+package com.edutech.courses.mapper;
+
+import com.edutech.common.dto.CourseCommentDTO;
+import com.edutech.courses.entity.CourseComment;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CourseCommentMapper {
+    CourseCommentDTO toDTO(CourseComment entity);
+    CourseComment toEntity(CourseCommentDTO dto);
+}
